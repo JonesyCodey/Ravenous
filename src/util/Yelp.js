8b5ecf0e06decfb3d07301853e3ca821c1b7335c
P Jones <pwallacejones@googlemail.com>
2017-12-09 10:30:50 +0000

    Yelp.js complete

---
+++ src/util/Yelp.js
@@ -0,0 +1,38 @@
+const clientId = "uiVwLFlKKgcXV1KVxKvckQ"
+const secret = "CDtVNlCCbNvwQUM-h6TbpmIZfyVGa1oVk8c6ycC0UDQO3JLCCTxyr1ky_9wdEd4hnVYfE6taMrJQDdPUq"
+
+let accessToken = 
+
+let Yelp = {
+	getAccessToken(){
+		if (accessToken){
+			return new Promise(resolve => resolve(accessToken));
+		}
+
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, 
+			{method:'POST'}).then(response=>{ return response.json();}).then(jsonResponse =>{accessToken=jsonResponse.access_token})
+	}
+search(term,location,sortBy){
+		return Yelp.getAccessToken().then(()=>{
+			return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+				{headers:{Authorization: `Bearer ${accessTocken}`}}).then(response=>{return response.json();}).then(jsonResponse => {
+					if(jsonResponse.businesses){
+						return jsonResponse.businesses.map(business => ({
+						id: business.id,
+          				imageSrc: business.image_url,
+          				name: business.name,
+          				address: business.location.address1,
+          				city: business.location.city,
+          				state: business.location.state,
+          				zipCode: business.location.zip_code,
+          				category: business.categories[0].title,
+          				rating: business.rating,
+          				reviewCount: business.review_count
+						
+						}))
+					}
+					})
+
+		});
+}
+}export default Yelp;
