bf1fc8acc95d8871cfcd23f89c013510753039ff
P Jones <pwallacejones@googlemail.com>
2017-12-09 10:46:04 +0000

    Final ammends to App.js and BL.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
+import Yelp from './util/Yelp.js'
 import SearchBar from './components/SearchBar/SearchBar';
 
-let business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-const businesses = [
-business, business, business, business, business, business];
 
 class App extends React.Component {
-  searchYelp(term,location,sortBy){
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  constructor(props){
+    this.state = {businesses:[]};
+
+    this.searchYelp.bind(this);
   }
+  searchYelp(term,location,sortBy){
+    Yelp.search(term, location,sortBy).then(businesess =>{
+      this.setState({businesses:businesses});
+    });
+  }
+
+  
   render() {
     return (
   <div className="App">
